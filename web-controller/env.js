55db180e2dd1dea2e7f444a2a4d6e1eb0d580037
@pjts13 <pierre.junior.tsoungui@mni.thm.de>
2025-08-19 15:45:51 +0200

    simon_game_projekt_sose24

--- web-controller/env.js
+++ web-controller/env.js
@@ -1,16 +1,7 @@
-/*window.__ENV__ = {
-    MQTT_BROKER_URL:      "iti-mqtt.mni.thm.de",
-    MQTT_BROKER_PORT:     "9001",
-    MQTT_USERNAME:        "group-24",
-    MQTT_PASSWORD:        "X4#sL8yNp1D",
-    MQTT_MESSAGE_PREFIX:  "group-24/",
-};*/
-
-
 window.__ENV__ = {
-    MQTT_BROKER_URL:      "iti-mqtt.mni.thm.de",
+    MQTT_BROKER_URL:      "localhost",
     MQTT_BROKER_PORT:     "9001",
-    MQTT_USERNAME:        "group-24",
-    MQTT_PASSWORD:        "X4k#sL8yNp1D",
+    MQTT_USERNAME:        "your_mqtt_username",
+    MQTT_PASSWORD:        "your_mqtt_password",
     MQTT_MESSAGE_PREFIX:  "group-24/",
 };
